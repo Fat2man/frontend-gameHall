51a8d2162737284663f65821b8323925aa949fe5
Fat2man <1250991840@qq.com>
2025-02-20 21:30:06 +0800

    '测试cicd'

--- src/views/login.vue
+++ src/views/login.vue
@@ -4,7 +4,7 @@
       <div class="forms-container">
         <div class="login-register-form login" :class="{ 'form-hidden': showRegister }">
           <div class="title-wrapper">
-            <div class="title">江江234大厅</div>
+            <div class="title">江江1大厅</div>
             <div class="title-decoration left"></div>
             <div class="title-decoration right"></div>
           </div>
